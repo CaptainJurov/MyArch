bd162ea1b426d09d3aa7b7a17e2194af6afbcf5e
CaptainJurov <goudazv@gmail.com>
2025-01-23 00:01:37 +0700

    Add files via upload

---
+++ rofi/launcher.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+## Author : Aditya Shakya (adi1090x)
+## Github : @adi1090x
+dir="$HOME/.config/rofi/"
+theme='style-8'
+
+## Run
+rofi \
+    -show drun \
+    -theme ${dir}/${theme}.rasi
